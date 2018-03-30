25aa252a5661d00ad56ac3813efddb63617dad9c
Derrick <stjohndlee@gmail.com>
2018-03-30 19:36:01 -0400

    update .gitignore

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -6,4 +6,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Rango says hello!")
+    return HttpResponse("Rango says hello! To go to the about page, click <a href='/rango/about/'> here </a>")
+
+def about(request):
+    return HttpResponse("This is the about page of the Rango app. \
+    To go to the main page, click <a href='/rango/'>here</a>")
