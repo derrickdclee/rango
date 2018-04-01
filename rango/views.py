c33b5ff749780b4bbac6233889196b12921b7ced
Derrick <stjohndlee@gmail.com>
2018-04-01 16:05:55 -0400

    wip on viewing categories with parametrized url

--- rango/views.py
+++ rango/views.py
@@ -4,12 +4,29 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from rango.models import Category, Page
+
 # Create your views here.
 def index(request):
-    context_dict = {'boldmessage' : "this is my strong message"}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
 
-    return render(request, 'rango/index.html', context=context_dict)
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
     return HttpResponse("This is the about page of the Rango app. \
     To go to the main page, click <a href='/rango/'>here</a>")
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['pages'] = None
+        context_dict['category'] = None
+
+    return render(request, 'rango/category.html', context_dict)
